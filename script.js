61d95d1926a28fb1323d12b0def668f02b5db12e
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-14 11:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+const s1 = [''];
+const s2 = '';
+
+const index = s1.findIndex(element => {
+  return element.toLowerCase() === s2.toLowerCase();
+});
+
+console.log(index); // 👉️ 1
+
+if (index !== -1) {
+  // 👉️ string is in the array
+}
+	
+
+
+	
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
